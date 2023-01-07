d32c995dc0171ba5ae414ed62e4262b0757fb982
Jeff Verkoeyen <jverkoey@gmail.com>
2023-01-06 21:29:42 -0800

    Implement basic login states.

--- ElectricSidecar/ElectricSidecar/ElectricSidecarApp.swift
+++ ElectricSidecar/ElectricSidecar/ElectricSidecarApp.swift
@@ -2,12 +2,47 @@ import SwiftUI
 
 @main
 struct ElectricSidecar: App {
-  @State var username: String = ""
+  @State var email: String = ""
   @State var password: String = ""
+
+  enum AuthState {
+    case loggedOut(error: Error?)
+    case authenticated
+  }
+  @State var authState: AuthState
+
+  init() {
+    if let userAuth = AuthStore.userAuth(), !userAuth.username.isEmpty && !userAuth.password.isEmpty {
+      self.email = userAuth.username
+      self.password = userAuth.password
+      authState = .authenticated
+    } else {
+      self.email = ""
+      self.password = ""
+      authState = .loggedOut(error: nil)
+    }
+  }
+
   var body: some Scene {
     WindowGroup {
-      LoginView(email: $username, password: $password) {
-        print("Did log in")
+      switch authState {
+      case .authenticated:
+        Text("Logged in")
+      case .loggedOut(let error):
+        ScrollView {
+          VStack {
+            if let error = error {
+              Text(error.localizedDescription)
+            }
+            LoginView(email: $email, password: $password) {
+              guard !email.isEmpty && !password.isEmpty else {
+                return
+              }
+              AuthStore.store(username: email, password: password)
+              authState = .authenticated
+            }
+          }
+        }
       }
     }
   }
