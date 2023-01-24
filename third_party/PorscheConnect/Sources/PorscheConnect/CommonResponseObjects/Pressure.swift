03f08548a822a78fef6f4bf4dcd2286d9f4444d9
Jeff Verkoeyen <jverkoey@gmail.com>
2023-01-23 23:27:36 -0800

    Unit is not optional in Pressure. Add BAR support.

--- third_party/PorscheConnect/Sources/PorscheConnect/CommonResponseObjects/Pressure.swift
+++ third_party/PorscheConnect/Sources/PorscheConnect/CommonResponseObjects/Pressure.swift
@@ -2,11 +2,12 @@ import Foundation
 
 public struct Pressure: Codable {
   public enum Unit: String, Codable {
+    case bar = "BAR"
     case psi = "PSI"
   }
 
   public let value: Double
-  public let unit: Unit?
+  public let unit: Unit
   public let valueInBar: Double
   public let unitTranslationKey: String
   public let unitTranslationKeyV2: String
