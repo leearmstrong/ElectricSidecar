7af769e9542176d02ce7e65c3f64b0bf82767957
Jeff Verkoeyen <jverkoey@gmail.com>
2023-01-23 22:11:46 -0800

    chargingTargetDateTime is optional in the emobility endpoint.

--- third_party/PorscheConnect/Sources/PorscheConnect/APIs/PorscheConnect+Emobility.swift
+++ third_party/PorscheConnect/Sources/PorscheConnect/APIs/PorscheConnect+Emobility.swift
@@ -73,7 +73,7 @@ public struct Emobility: Codable {
     public let remainingChargeTimeUntil100PercentInMinutes: Int?
     public let remainingERange: Distance
     public let remainingCRange: String?  // TBD while charging
-    public let chargingTargetDateTime: String  //2021-02-19T01:09
+    public let chargingTargetDateTime: String?  //2021-02-19T01:09
     public let status: String?  // TBD while charging
     public let chargeRate: ChargeRate
     public let chargingPower: Double
