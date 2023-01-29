04cd01e4c0715711b5bf1dcf7ce4ec049d6175e6
Jeff Verkoeyen <jverkoey@gmail.com>
2023-01-28 23:37:18 -0800

    Consolidate complication view logic into the widget file.

--- ElectricSidecar/ESComplications/VehicleCharge/VehicleChargeTimelineProvider.swift
+++ ElectricSidecar/ESComplications/VehicleCharge/VehicleChargeTimelineProvider.swift
@@ -7,6 +7,12 @@ private final class Storage {
   var lastKnownChargingState: Bool?
 }
 
+struct ChargeRemainingTimelineEntry: TimelineEntry {
+  let date: Date
+  let chargeRemaining: Double?
+  let isCharging: Bool?
+}
+
 struct ChargeRemainingTimelineProvider: TimelineProvider {
   typealias Entry = ChargeRemainingTimelineEntry
 
@@ -58,109 +64,5 @@ struct ChargeRemainingTimelineProvider: TimelineProvider {
       )], policy: .after(.now.addingTimeInterval(60 * 30)))
       completion(timeline)
     }
-  }
-}
-
-struct ChargeRemainingTimelineEntry: TimelineEntry {
-  let date: Date
-  let chargeRemaining: Double?
-  let isCharging: Bool?
-}
-
-struct VehicleChargeEntryView : View {
-  @Environment(\.widgetFamily) var family
-  @Environment(\.widgetRenderingMode) var widgetRenderingMode
-
-  let entry: ChargeRemainingTimelineProvider.Entry
-
-  var body: some View {
-
-    switch family {
-    case .accessoryCircular:
-      ChargeView(
-        batteryLevel: entry.chargeRemaining,
-        isCharging: entry.isCharging == true,
-        lineWidth: 4
-      )
-      .padding(2)
-    case .accessoryCorner:
-      if let chargeRemaining = entry.chargeRemaining {
-        HStack(spacing: 0) {
-          Image(entry.isCharging == true ? "taycan.charge" : "taycan")
-            .font(.system(size: WKInterfaceDevice.current().screenBounds.width < 195 ? 23 : 26))
-            .fontWeight(.regular)
-        }
-        .widgetLabel {
-          Gauge(value: chargeRemaining, in: 0...100.0) {
-            Text("")
-          } currentValueLabel: {
-            Text("")
-          } minimumValueLabel: {
-            Text("")
-          } maximumValueLabel: {
-            Text(chargeRemaining < 100 ? Self.formatted(chargeRemaining: chargeRemaining) : "100")
-              .foregroundColor(batteryColor)
-          }
-          .tint(batteryColor)
-          .gaugeStyle(LinearGaugeStyle(tint: Gradient(colors: [.red, .orange, .yellow, .green])))
-        }
-      } else {
-        HStack(spacing: 0) {
-          Image(entry.isCharging == true ? "taycan.charge" : "taycan")
-            .font(.system(size: WKInterfaceDevice.current().screenBounds.width < 195 ? 23 : 26))
-            .fontWeight(.regular)
-        }
-      }
-    case .accessoryInline:
-      // Note: inline accessories only support one Text and/or Image element. Any additional
-      // elements will be ignored.
-      HStack {
-        if widgetRenderingMode == .fullColor {
-          Image(systemName: "bolt.car")
-            .symbolRenderingMode(.palette)
-            .foregroundStyle(entry.isCharging == true ? .white : .clear, .white)
-        } else {
-          // Non-full-color rendering modes don't support palette rendering, so we need to use
-          // an alternate glyph instead.
-          Image(systemName: entry.isCharging == true ? "bolt.car" : "car")
-        }
-        if let chargeRemaining = entry.chargeRemaining {
-          Text(Self.formatted(chargeRemaining: chargeRemaining))
-        }
-      }
-    default:
-      Text("Unsupported")
-    }
-  }
-
-  var batteryColor: Color {
-    guard let chargeRemaining = entry.chargeRemaining else {
-      return .gray
-    }
-    if chargeRemaining >= 80 {
-      return .green
-    } else if chargeRemaining >= 50 {
-      return .yellow
-    } else if chargeRemaining > 20 {
-      return .orange
-    } else {
-      return .red
-    }
-  }
-
-  static func formatted(chargeRemaining: Double) -> String {
-    return String(format: "%.0f%%", chargeRemaining)
-  }
-}
-
-struct VehicleChargeWidget_Previews: PreviewProvider {
-  static var previews: some View {
-    VehicleChargeEntryView(entry: ChargeRemainingTimelineEntry(
-      date: Date(),
-      chargeRemaining: 100,
-      isCharging: true
-    ))
-    .previewDevice("Apple Watch Series 8 (45mm)")
-    .previewContext(WidgetPreviewContext(family: .accessoryCircular))
   }
 }
--- ElectricSidecar/ESComplications/VehicleCharge/VehicleChargeWidget.swift
+++ ElectricSidecar/ESComplications/VehicleCharge/VehicleChargeWidget.swift
@@ -7,10 +7,108 @@ struct VehicleChargeWidget: Widget {
       kind: "ESComplications.VehicleCharge",
       provider: ChargeRemainingTimelineProvider()
     ) { entry in
-        VehicleChargeEntryView(entry: entry)
+      WidgetView(entry: entry)
+    }
+    .configurationDisplayName("Charge")
+    .description("Show the remaining charge on your vehicle")
+    .supportedFamilies([.accessoryCircular, .accessoryCorner, .accessoryInline])
+  }
+}
+
+private struct WidgetView : View {
+  @Environment(\.widgetFamily) var family
+  @Environment(\.widgetRenderingMode) var widgetRenderingMode
+
+  let entry: ChargeRemainingTimelineProvider.Entry
+
+  var body: some View {
+
+    switch family {
+    case .accessoryCircular:
+      ChargeView(
+        batteryLevel: entry.chargeRemaining,
+        isCharging: entry.isCharging == true,
+        lineWidth: 4
+      )
+      .padding(2)
+    case .accessoryCorner:
+      if let chargeRemaining = entry.chargeRemaining {
+        HStack(spacing: 0) {
+          Image(entry.isCharging == true ? "taycan.charge" : "taycan")
+            .font(.system(size: WKInterfaceDevice.current().screenBounds.width < 195 ? 23 : 26))
+            .fontWeight(.regular)
+        }
+        .widgetLabel {
+          Gauge(value: chargeRemaining, in: 0...100.0) {
+            Text("")
+          } currentValueLabel: {
+            Text("")
+          } minimumValueLabel: {
+            Text("")
+          } maximumValueLabel: {
+            Text(chargeRemaining < 100 ? Self.formatted(chargeRemaining: chargeRemaining) : "100")
+              .foregroundColor(batteryColor)
+          }
+          .tint(batteryColor)
+          .gaugeStyle(LinearGaugeStyle(tint: Gradient(colors: [.red, .orange, .yellow, .green])))
+        }
+      } else {
+        HStack(spacing: 0) {
+          Image(entry.isCharging == true ? "taycan.charge" : "taycan")
+            .font(.system(size: WKInterfaceDevice.current().screenBounds.width < 195 ? 23 : 26))
+            .fontWeight(.regular)
+        }
       }
-      .configurationDisplayName("Charge")
-      .description("Show the remaining charge on your vehicle")
-      .supportedFamilies([.accessoryCircular, .accessoryCorner, .accessoryInline])
+    case .accessoryInline:
+      // Note: inline accessories only support one Text and/or Image element. Any additional
+      // elements will be ignored.
+      HStack {
+        if widgetRenderingMode == .fullColor {
+          Image(systemName: "bolt.car")
+            .symbolRenderingMode(.palette)
+            .foregroundStyle(entry.isCharging == true ? .white : .clear, .white)
+        } else {
+          // Non-full-color rendering modes don't support palette rendering, so we need to use
+          // an alternate glyph instead.
+          Image(systemName: entry.isCharging == true ? "bolt.car" : "car")
+        }
+        if let chargeRemaining = entry.chargeRemaining {
+          Text(Self.formatted(chargeRemaining: chargeRemaining))
+        }
+      }
+    default:
+      Text("Unsupported")
+    }
+  }
+
+  var batteryColor: Color {
+    guard let chargeRemaining = entry.chargeRemaining else {
+      return .gray
+    }
+    if chargeRemaining >= 80 {
+      return .green
+    } else if chargeRemaining >= 50 {
+      return .yellow
+    } else if chargeRemaining > 20 {
+      return .orange
+    } else {
+      return .red
+    }
+  }
+
+  static func formatted(chargeRemaining: Double) -> String {
+    return String(format: "%.0f%%", chargeRemaining)
+  }
+}
+
+struct VehicleChargeWidget_Previews: PreviewProvider {
+  static var previews: some View {
+    WidgetView(entry: ChargeRemainingTimelineEntry(
+      date: Date(),
+      chargeRemaining: 100,
+      isCharging: true
+    ))
+    .previewDevice("Apple Watch Series 8 (45mm)")
+    .previewContext(WidgetPreviewContext(family: .accessoryCircular))
   }
 }
