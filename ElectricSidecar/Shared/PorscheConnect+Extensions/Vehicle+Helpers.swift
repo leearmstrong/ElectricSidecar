b3610efd846b659e11309c021fb291afc7588943
Jeff Verkoeyen <jverkoey@gmail.com>
2023-02-04 18:50:47 -0800

    Standardize the camera view names.

--- ElectricSidecar/Shared/PorscheConnect+Extensions/Vehicle+Helpers.swift
+++ ElectricSidecar/Shared/PorscheConnect+Extensions/Vehicle+Helpers.swift
@@ -9,21 +9,12 @@ extension Vehicle {
   }
   var personalizedPhoto: VehiclePicture? {
     return pictures?.first(where: { picture in
-      picture.view == "personalized" && picture.size == 2
+      picture.view == .personalized && picture.size == 2
     })
   }
-  enum Camera: String {
-    case front = "extcam01"
-    case side = "extcam02"
-    case rear = "extcam03"
-    case topAngled = "extcam04"
-    case overhead = "extcam05"
-    case dashboard = "intcam01"
-    case cabin = "intcam02"
-  }
-  func externalCamera(_ camera: Camera, size: Int) -> VehiclePicture? {
+  func externalCamera(_ camera: VehiclePicture.CameraView, size: Int) -> VehiclePicture? {
     return pictures?.first(where: { picture in
-      picture.view == camera.rawValue && picture.size == size
+      picture.view == camera && picture.size == size
     })
   }
 }
